b3e0b609913d64bebc775fffd3484ac87ec3f9e3
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-07-03 12:39:05 +0530

    Exercise

---
+++ Basics/Variable/Example.py
@@ -0,0 +1,5 @@
+#Making calculator that add two number and show sum
+var1 =input("Enter the number: ")
+var2 = input("Enter the second number: ")
+var3 = int(var1) +int(var2)
+print("The sum is:", var3)
