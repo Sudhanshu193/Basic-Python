8ec3d1a036916471061089f7dddb2ae2871e1f7e
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-07-03 12:30:39 +0530

    input and output

---
+++ Basics/Variable/input.py
@@ -0,0 +1,4 @@
+print("Enter the number: ")
+inpnum = input()
+#print("You entered :" ,inpnum +10)# here it will show error because input take value as a string datatype by defult
+print(int(inpnum)*10)
