9a6d3a593e9d426521074a3daa37ecb8592d4dc1
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-07-04 14:54:31 +0530

    string

---
+++ Basics/Data type/slicing-string.py
@@ -0,0 +1,14 @@
+A ="we are going to slice the sentence"
+print (len(A))
+print(A[4:20]) #here we slice the sentence from index 4 to index 19
+# (index 20 does not include)
+
+#slicing from end
+print(A[:30])#here index till index 30 it works.
+
+#slicing from start
+print(A[17:])#here it goes to end
+
+#negative indexing
+print(A[-20:-2])# it start from last index as -1;
+
