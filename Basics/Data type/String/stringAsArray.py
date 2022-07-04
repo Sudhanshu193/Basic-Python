4a66cc73894e2c97e745f91a627dc441f4c423cb
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-07-04 13:25:33 +0530

    String

---
+++ Basics/Data type/String/stringAsArray.py
@@ -0,0 +1,20 @@
+#accessing index element of string
+a = "hello world my name is sudhanshu"
+print(a[6])
+
+
+#looping through string
+for x in "aloha":
+    print(x)
+
+#string length
+print(len(a))
+
+#checking weather word is present in string or not
+print("bye" in a) 
+
+#if and if NOT
+if "my" in a:
+    print("yes it present")
+
+print("expensive" not in a)
